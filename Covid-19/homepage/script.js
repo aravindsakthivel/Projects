0b230b91a001060b627a4662facce1921db8f88f
AravindSakthivel <aravind.sakthi@outlook.com>
2020-06-29 22:16:33 +0530

    news bar

--- Covid-19/homepage/script.js
+++ Covid-19/homepage/script.js
@@ -1,3 +1,43 @@
-window.addEventListener('load',function(){
+async function headlines(){
+    try{
+        console.log(10)
+        var newsApi = "https://api.smartable.ai/coronavirus/news/global"
+        const options = {
+            headers:{'Subscription-Key': '3009d4ccc29e4808af1ccc25c69b4d5d'}
+        }
+        var newsData = await getHeadlinesData(newsApi, options)
+        newsData.news.forEach(element => {
+            // console.log(element)
+            newsRenderDom(element)
+        });
+    }
+    catch (error){
+        console.log(error)
+    }
     
+}
+
+const getHeadlinesData = (api, data) =>{
+    return fetch(api, data).then(response => response.json())
+}
+
+const newsRenderDom = (data) => {
+    var newsHolder = document.getElementById('news_inner')
+    console.log(data)
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    console.log(randomColor)
+    newsHolder.innerHTML += `<div class="carousel-item">
+    <div class="card card_caro shadow p-0 mb-5 bg-white rounded"">
+        <div class="card-body">
+            <small>${data.excerpt}</small>
+        </div>
+    </div>`
+}
+
+
+
+
+window.addEventListener('load',function(){
+    headlines()
 })
+
